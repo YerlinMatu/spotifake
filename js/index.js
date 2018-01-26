85afd31607bb4641bb3eb201a676608ae95ac6dc
Yerlin Matu <yerlinmatu@gmail.com>
2018-01-26 18:48:00 -0500

    :join:

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+  el: 'main',
+  data: {
+    profiles: [
+      {
+        name: 'Jhon Doe',
+        imgLink: 'https://static.pexels.com/photos/220453/pexels-photo-220453.jpeg',
+        rol: 'Music'
+      },
+      {
+        name: 'Yerlin Matu',
+        imgLink: 'https://static.pexels.com/photos/614810/pexels-photo-614810.jpeg',
+        rol: 'BeatMaker'
+      },
+      {
+        name: 'Charles Jobs',
+        imgLink: 'https://static.pexels.com/photos/247885/pexels-photo-247885.jpeg',
+        rol: 'Produccer'
+      }
+    ]
+  }
+})
